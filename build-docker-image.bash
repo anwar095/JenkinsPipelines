3f25905b1a2ad34156831ff6f719b853150dc6d0
anwar095 <54428215+anwar095@users.noreply.github.com>
2022-09-27 19:05:40 +0530

    Update build-docker-image.bash

--- build-docker-image.bash
+++ build-docker-image.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Login into Docker repo"
-docker login -u anwar095  -p 0167768@Anw https://github.com/
+docker login -u anwar095  -p 0167768@Anw https://hub.docker.com/?code=nYTm7I9B5K8nSO8joHD71Q9hJEGzV_icfboEPyDi1M3h6&state=V1Z4cFhlZkd4YVZfTTlZYjRPUWZMck13N1FLWmdENWlHaURrcktXTUgudg%3D%3D
 echo "Building Docker Image"
 docker build -t sprintpetclinic .
 
